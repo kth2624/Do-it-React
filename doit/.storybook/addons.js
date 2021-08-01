a05f98b1ae5761a5f2f7ed44a001df0216052c4a
TAEHO KIM <39645477+kth2624@users.noreply.github.com>
2021-08-01 19:46:44 +0900

    8/1 04-02장

---
+++ doit/.storybook/addons.js
@@ -0,0 +1,4 @@
+//확장 도구는 이곳에 추가된다.
+
+import '@storybook/addon-actions/register';
+import 'storybook-addon-jsx/register';
--- doit/src/App.js
+++ doit/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import './sass/materialize.scss';
 //import StateExample from './03/StateExample';
 import Counter from './03/Counter';
 function App() {
   return (
-    <div className="body">
-      <Counter/>
+    <div>
+      <nav>
+        <div className="nav-wrapper">
+          <div>두잇! 리액트 시작하기</div>
+        </div>
+      </nav>
+      <h1>머터리얼 CSS</h1>
     </div>
   );
 }
